96f801c9017c243a210d017b92715e245d07bd89
Miguel Palhas <mpalhas@gmail.com>
2014-02-07 11:26:46 +0000

    All green

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
   Tinystore::Application.config.session_store :cookie_store, key: '_tinystore_session', domain: '.lvh.me'
 else
   Tinystore::Application.config.session_store :cookie_store, key: '_tinystore_session', domain: '.tinystore.me' # TODO check this later
