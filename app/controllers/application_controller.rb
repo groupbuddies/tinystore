403ee759b30bce4706be76e369cea2208503fa22
Miguel Palhas <mpalhas@gmail.com>
2014-02-14 17:40:36 +0000

    Quickfixes to back-end layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :exit_store, if: :devise_controller?
   before_filter :configure_devise_parameters, if: :devise_controller?
   before_filter :sanitize_cart, if: :is_client?
 
@@ -15,6 +16,10 @@ class ApplicationController < ActionController::Base
 
   def configure_devise_parameters
     devise_parameter_sanitizer.for(:sign_up).concat [:name, store_attributes: [:name, :slug, :email, :logo, :description]]
+  end
+
+  def exit_store
+    redirect_to url_for(params.merge(subdomain: '')) if current_store.present?
   end
 
   def sanitize_cart
