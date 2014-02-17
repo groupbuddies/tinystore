acda7aae44788b1ee41a60a506dd4c997845d760
Miguel Palhas <mpalhas@gmail.com>
2014-02-17 14:51:34 +0000

    Disable letter opener for video

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,5 @@ Tinystore::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { host: 'lvh.me', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
 end
